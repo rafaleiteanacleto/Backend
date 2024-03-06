47eb95551918c80fa2b5edcca6ffd1d8ff3abaa8
Rafaela Leite a Silva <rafaelalds135@gmail.com>
2024-03-06 20:15:04 -0300

    AULA BACKEND

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/hello',function(req, res){
     res.send('Chegou a resposta')             
 })
 app.get('/nome',function(req, res){
-    res.send('Lucas Fernandes')             
+    res.send('Rafa Leite')             
 })
 
 app.listen(3000, function(){
