e963f3714df2f8d984c59721f0b54e754e8f6d62
Lucas Fernandes da silva <lucas_fernadesss@hotmail.com>
2024-03-06 19:47:37 -0300

    NODE AULA 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/',function(req, res){
+    res.send('Rota Principal')             
+})
+app.get('/hello',function(req, res){
+    res.send('Chegou a resposta')             
+})
+app.get('/nome',function(req, res){
+    res.send('Lucas Fernandes')             
+})
+
+app.listen(3000, function(){
+    console.log('servidor aberto na porta 3000')
+})
